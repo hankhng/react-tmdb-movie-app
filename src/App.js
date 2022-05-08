fb3a21b042aae7273caf7854263eb60ca3cad1cd
Hank H'ng <hankhng@hotmail.com>
2022-05-08 22:56:20 +0100

    Setup app + add use effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const MOVIE_API = "https://api.themoviedb.org/3/"
+
+  const fetchMovies = async () => {
+    const data = await axios.get(`${MOVIE_API}`)
+  }
+
+  useEffect(() => {}
+  , []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
